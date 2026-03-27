140ece0a80c5c983f50433437a487a1ef6388dac
Pietro Romano <pietroro98@gmail.com>
2026-03-27 12:33:51 +0100

    aggiornamento service cartrlla

--- src/main/java/it/prova/cartellaesattoriale/service/CartellaEsattorialeService.java
+++ src/main/java/it/prova/cartellaesattoriale/service/CartellaEsattorialeService.java
@@ -1,4 +1,13 @@
 package it.prova.cartellaesattoriale.service;
+import it.prova.cartellaesattoriale.model.CartellaEsattoriale;
+import java.util.List;
 
-public interface CartellaEsattorialeService {
+public interface CartellaEsattorialeService
+{
+    List<CartellaEsattoriale> listAll(boolean eager);
+    CartellaEsattoriale caricaSingola(Long id);
+    CartellaEsattoriale caricaSingolaEager(Long id);
+    CartellaEsattoriale inserisci(CartellaEsattoriale cartella);
+    CartellaEsattoriale aggiorna(CartellaEsattoriale cartella);
+    void rimuovi(Long id);
 }
--- src/main/java/it/prova/cartellaesattoriale/service/CartellaEsattorialeServiceImpl.java
+++ src/main/java/it/prova/cartellaesattoriale/service/CartellaEsattorialeServiceImpl.java
@@ -1,4 +1,52 @@
 package it.prova.cartellaesattoriale.service;
 
-public class CartellaEsattorialeServiceImpl {
+import it.prova.cartellaesattoriale.model.CartellaEsattoriale;
+import it.prova.cartellaesattoriale.repository.CartellaEsattorialeRepository;
+import it.prova.cartellaesattoriale.web.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class CartellaEsattorialeServiceImpl implements CartellaEsattorialeService
+{
+    @Autowired
+    private CartellaEsattorialeRepository cartellaEsattorialeRepository;
+
+    @Override
+    public List<CartellaEsattoriale> listAll(boolean eager) {
+        return eager ? cartellaEsattorialeRepository.findAllEager() : cartellaEsattorialeRepository.findAll();
+    }
+
+    @Override
+    public CartellaEsattoriale caricaSingola(Long id) {
+        return cartellaEsattorialeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public CartellaEsattoriale caricaSingolaEager(Long id) {
+        return cartellaEsattorialeRepository.findSingleEager(id);
+    }
+
+    @Override
+    @Transactional
+    public CartellaEsattoriale inserisci(CartellaEsattoriale cartella) {
+        return cartellaEsattorialeRepository.save(cartella);
+    }
+
+    @Override
+    @Transactional
+    public CartellaEsattoriale aggiorna(CartellaEsattoriale cartella) {
+        return cartellaEsattorialeRepository.save(cartella);
+    }
+
+    @Override
+    public void rimuovi(Long id) {
+        cartellaEsattorialeRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Cartella esattoriale non trovata con id: " + id));
+        cartellaEsattorialeRepository.deleteById(id);
+    }
 }
